71847498e64c1a7e6377afa4670fbc9dfebac639
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2022-10-01 22:17:44 +0600

    1 day practice completed

--- basic.js
+++ basic.js
@@ -1,2 +1,8 @@
 console.log("we are practice basic javascript")
 
+var name = "Gaus Al Munir Tushar";
+var age = 20;
+var address = "Rangpur";
+console.log(name);
+console.log(age);
+console.log(address);
